7529789f57e4ad19ffe36e732c0e25a6d0b4614b
Kim <kbh3542@naver.com>
2021-12-11 22:45:50 +0900

    .

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 money = pd.read_csv("money.csv",encoding='cp949')
 
+print("<<<<<<<<<<<<<법인의 측면>>>>>>>>>>>")
+
 #사고원인별 법인사고건수의 평균
 Aver_by_region = money.groupby(['사고원인'])['법인사고건수'].mean()
 print(Aver_by_region)
@@ -8,8 +10,6 @@ print(Aver_by_region)
 #사고원인별 법인사고금액(억원)
 Aver_by_region = money.groupby(['사고원인'])['법인사고금액(억원)'].mean()
 print(Aver_by_region)
-
-print("<<<<<<<<<<<<<법인의 측면>>>>>>>>>>>")
 
 #보증종류별 법인 사고 건수
 Aver_by_region = money.groupby(['보증종류'])['법인사고건수'].mean()
@@ -42,7 +42,6 @@ print(Aver_by_region)
 #보증종류별 개인사고금액(억원)
 Aver_by_region = money.groupby(['보증종류'])['개인사고금액(억원)'].mean()
 print(Aver_by_region)
-
 
 #개인 사고 건수 10순위
 print(money.sort_values(by=['개인사고건수'],ascending=False).head(10))
